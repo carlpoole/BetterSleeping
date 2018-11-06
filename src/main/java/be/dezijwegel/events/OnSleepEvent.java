1b8c91dd7cd47195c243deeb2bef5fb6c246f479
MetamorphicMike <email@carl.codes>
2018-11-06 11:38:49 -0600

    Cleanup, changed playersNeeded calculation to avoid miscalculations and cleaner implementation

--- src/main/java/be/dezijwegel/events/OnSleepEvent.java
+++ src/main/java/be/dezijwegel/events/OnSleepEvent.java
@@ -3,7 +3,6 @@ package be.dezijwegel.events;
 import be.dezijwegel.bettersleeping.BetterSleeping;
 import be.dezijwegel.bettersleeping.Reloadable;
 import be.dezijwegel.files.FileManagement;
-import java.util.logging.Level;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.World;
@@ -15,89 +14,84 @@ import org.bukkit.event.player.PlayerBedEnterEvent;
 import org.bukkit.event.player.PlayerBedLeaveEvent;
 
 /**
- *
  * @author Dieter Nuytemans
  */
 public class OnSleepEvent implements Listener, Reloadable {
-    
+
     private BetterSleeping plugin;
-    
-    private int playersNeeded;
+
+    private int percentNeeded;
     private int playersSleeping;
     private long sleepDelay;
     private FileManagement configFile;
     private FileManagement langFile;
-    
+
     private String prefix;
     private String enough_sleeping;
     private String amount_left_plural;
     private String amount_left_single;
 
-    public OnSleepEvent(FileManagement configFile, FileManagement langFile, BetterSleeping plugin)
-    {
+    public OnSleepEvent(FileManagement configFile, FileManagement langFile, BetterSleeping plugin) {
         this.plugin = plugin;
-        
+
         this.configFile = configFile;
         this.langFile = langFile;
-        
+
         reload();
     }
-    
+
     @EventHandler
-    public void onPlayerEnterBed(PlayerBedEnterEvent e)
-    {
+    public void onPlayerEnterBed(PlayerBedEnterEvent e) {
         playersSleeping++;
-        float numNeeded = playersNeeded();
-        
-        if (playersSleeping >= numNeeded)
-        {
+        int numNeeded = playersNeeded();
+
+        if (playersSleeping >= numNeeded) {
             Bukkit.getServer().getScheduler().runTaskLater(plugin, () -> {
                 if (playersSleeping >= numNeeded) {
-                    for(World world : Bukkit.getWorlds()) {
+                    for (World world : Bukkit.getWorlds()) {
                         world.setStorm(false);
                         world.setTime(1000);
                     }
 
-                    for (Player p : Bukkit.getOnlinePlayers())
-                    {
+                    for (Player p : Bukkit.getOnlinePlayers()) {
                         p.sendMessage(prefix + enough_sleeping);
                     }
                 }
             }, sleepDelay);
-                    
+
         } else {
-            float numLeft = numNeeded - playersSleeping;
+            int numLeft = numNeeded - playersSleeping;
             String msg = "";
 
-            if (numLeft > 1 ) {
-                msg = amount_left_plural.replaceAll("<amount>", Integer.toString(Math.round(numLeft)));
+            if (numLeft > 1) {
+                msg = amount_left_plural.replaceAll("<amount>", Integer.toString(numLeft));
             } else if (numLeft > 0) {
-                msg = amount_left_single.replaceAll("<amount>", Integer.toString(Math.round(numLeft)));
+                msg = amount_left_single.replaceAll("<amount>", Integer.toString(numLeft));
             }
 
-            if(!msg.isEmpty()){
-                for (Player p : Bukkit.getOnlinePlayers())
-                {
+            if (!msg.isEmpty()) {
+                for (Player p : Bukkit.getOnlinePlayers()) {
                     p.sendMessage(prefix + msg);
                 }
             }
         }
     }
-    
+
     @EventHandler
-    public void onPlayerLeaveBed(PlayerBedLeaveEvent e)
-    {
+    public void onPlayerLeaveBed(PlayerBedLeaveEvent e) {
         playersSleeping--;
     }
-    
+
     /**
      * Calculate the amount of players needed according to the settings and current online players
+     *
      * @return float
      */
-    public float playersNeeded()
-    {
+    private int playersNeeded() {
+        // Todo: this needs to only reflect overworld players
         int numOnline = Bukkit.getOnlinePlayers().size();
-        return (playersNeeded * numOnline / 100.0f);
+        float needed = (percentNeeded * numOnline / 100.0f);
+        return (int) Math.ceil(needed);
     }
 
     /**
@@ -114,24 +108,24 @@ public class OnSleepEvent implements Listener, Reloadable {
             sleepDelay = 40;
             configFile.forceDefaultConfig();
         }
-        
+
         if (configFile.contains("percentage_needed")) {
-            
-            playersNeeded = configFile.getInt("percentage_needed");
-        
-            if (playersNeeded > 100) playersNeeded = 100;
-            else if (playersNeeded < 1) playersNeeded = 1;
-            
-        } else playersNeeded = 30;
-        
+
+            percentNeeded = configFile.getInt("percentage_needed");
+
+            if (percentNeeded > 100) percentNeeded = 100;
+            else if (percentNeeded < 1) percentNeeded = 1;
+
+        } else percentNeeded = 30;
+
         if (langFile.contains("prefix"))
             prefix = langFile.getString("prefix");
         else prefix = "§6[BetterSleeping] §3";
-        
+
         if (langFile.contains("enough_sleeping"))
             enough_sleeping = langFile.getString("enough_sleeping");
         else enough_sleeping = "Enough people are sleeping now!";
-        
+
         if (langFile.contains("amount_left_plural"))
             amount_left_plural = langFile.getString("amount_left_plural");
         else amount_left_plural = "There are <amount> more people needed to skip the night/storm!";
